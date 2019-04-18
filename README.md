b6bbf26725d085f1f60c1bad56c9ecd34e3cb8e6
Stefan Vorkoetter <stefan@capable.ca>
2019-04-17 20:55:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# piblank - GPIO to Key Presses and Display Power Management
+
+The `piblank` utility is a screen saver daemon for use with Raspbian, intended
+to be launched from your X11 session manager's auto-start file. It monitors X11
+user idle time (no input from the keyboard or mouse) and turns off the display
+when idle, turning it back on when the user interacts with the system.
+
+## System Requirements
+
+The following libraries are used by `piblank` (some of these might already be
+installed by default):
+
+* libx11-6
+* libxss1
+* libraspberrypi-bin for the `vcgencmd` utility
+
+If you want to rebuild `piblank` from source, you will also need the following:
+
+* libx11-dev
+* libxss-dev
+
+## Usage
+
+The `piblank` command should be invoked from
+`~/.config/lxsession/LXDE-pi/autostart` or its equivalent if you're using a
+different window manager than LXDE. In any case, it shouldn't be launched until
+after your X11 session is up and running.
+
+The command line syntax for `piblank` is as follows. All arguments are
+optional.
+
+    piblank [-n] [timeout]
+
+By default, `piblank` runs as a daemon. The `piblank` command will return
+immediately, but the daemon will continue to run in the background. If the `-n`
+option is given, then `piblank` will remain in the foreground until you
+terminate it by pressing `Ctrl+C`.
+
+The _timeout_ argument specifies the time, in minutes, that the system must be
+idle before the display is turned off. If not specified, the default is 30
+minutes.
+
+## Internals
+
+Screen blanking and unblanking is done using the `vcgencmd` utility, invoked as
+`vcgencmd display_power x`, where _x_ is 0 or 1 respectively.
+
+Idle time determination is done using the `XScreenSaverQueryInfo` function of
+the `libxss` library.
---
+++ main.c
@@ -0,0 +1,108 @@
+/* piblank - Raspberry Pi Screen Power Management
+
+   Copyright (c) 2019 by Stefan Vorkoetter
+
+   This file is part of piblank.
+
+   piblank is free software: you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation, either version 3 of the License, or (at your option) any later
+   version.
+
+   piblank is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+   details.
+
+   You should have received a copy of the GNU General Public License along with
+   piblank. If not, see <http://www.gnu.org/licenses/>. */
+
+#define _BSD_SOURCE	// To enable daemon and usleep functions.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <errno.h>
+#include <getopt.h>
+#include <unistd.h>
+
+#include <X11/Xlib.h>
+#include <X11/extensions/scrnsaver.h>
+#include <X11/extensions/XTest.h>
+
+/* Command line options (in the form expected by getopt). */
+#define OPTIONS "n"
+
+static void usage( void )
+{
+    /* Print usage information and exit. */
+    fprintf(stderr,"Usage: piblank [%s] timeout\n",OPTIONS);
+    fprintf(stderr,"-n\t- do not become a daemon, remain in the foreground\n");
+    fprintf(stderr,"timeout\t- time in whole minutes (1 to 10000) until display power off (default 30)\n");
+    exit(1);
+}
+
+int main( int argc, char **argv )
+{
+    /* Process command line options. */
+    bool optDaemonize = true;
+    int c;
+    while( (c = getopt(argc,argv,OPTIONS)) != -1 ) {
+	switch( c ) {
+	case 'n':
+	    optDaemonize = false;
+	    break;
+	default:
+	    usage();
+	}
+    }
+
+    /* Get idle timeout value and convert to milliseconds. */
+    int timeout = 30;
+    if( optind < argc ) {
+        if( optind != argc - 1 )
+	    usage();
+	timeout = atoi(argv[optind]);
+	if( timeout < 1 || timeout > 10000 )
+	    usage();
+    }
+    timeout *= 60000;
+
+    /* Do what it takes to become a daemon. */
+    if( optDaemonize && daemon(0,0) != 0 ) {
+	fprintf(stderr,"piblank: failed to become a daemon: %s\n",strerror(errno));
+	return( 1 );
+    }
+
+    /* Check for idle timeout every 500ms. */
+    bool screenOn = true;
+    Display *display = NULL;
+    for( ;; ) {
+	usleep(500000);
+
+	/* Look up user idle time in milliseconds. Based on code found at
+	   https://superuser.com/questions/638357 */
+	if( display == NULL && (display = XOpenDisplay(NULL)) == NULL )
+	    continue;
+
+	int event_base, error_base;
+	if( !XScreenSaverQueryExtension(display,&event_base,&error_base) )
+	    continue;
+
+	XScreenSaverInfo info;
+	XScreenSaverQueryInfo(display,DefaultRootWindow(display),&info);
+
+	/* Turn screen on or off appropriately. */
+	if( screenOn && info.idle >= timeout ) {
+	    system("vcgencmd display_power 0");
+	    screenOn = false;
+	}
+	else if( !screenOn && info.idle < timeout ) {
+	    system("vcgencmd display_power 1");
+	    screenOn = true;
+	}
+    }
+
+    return( 0 );
+}
